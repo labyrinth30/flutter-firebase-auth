6decb56a046a33d66bce2f5ef8753add211ec27a
Lee, Younha <younha0088@gmail.com>
2023-08-31 02:27:32 +0900

    파이어베이스 설치

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_auth/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Container();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: home_screen(),
+    );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+
+class home_screen extends StatelessWidget {
+  const home_screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello, World!'),
+      ),
+    );
+  }
+}
